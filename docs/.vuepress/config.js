f9985d856adc2e52876054b19c226c4c9b3aa5e4
zhuangchen <zhuangchen@zto.cn>
2019-10-23 15:25:56 +0800

    确定模块

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,39 @@
 module.exports = {
   base: '/',
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }]
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   markdown: {
     lineNumbers: true  // 代码行号显示
   },
+  // 格外监听的文件
+  extraWatchFiles: [
+  ],
+  // 插件
+  plugins: [
+    '@vuepress/back-to-top',
+    '@vuepress/active-header-links',
+    [
+      '@vuepress/last-updated'
+    ],
+    '@vuepress/nprogress',
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ]
+  ],
+  // 多语言配置
   locales: {
     '/en/': {
       lang: 'en-US',
@@ -19,6 +47,26 @@ module.exports = {
     }
   },
   themeConfig: {
+    algolia: {
+      apiKey: '<API_KEY>',
+      indexName: '<INDEX_NAME>'
+    },
+    nav: [
+      { text: '主页', link: '/' },
+      {
+        text: '学习笔记',
+        ariaLabel: '学习笔记 菜单',
+        items: [
+          { text: '前端开发', link: '/note/frontend/' },
+          { text: '后端开发', link: '/note/backend/' },
+          { text: '工具', link: '/note/tool/' },
+          { text: '小技巧', link: '/note/tip/' }
+        ]
+      },
+      { text: '图片分享', link: '/share/pic/' },
+      { text: '记录生活', link: '/life/' },
+      { text: '查看源码', link: 'https://github.com/babybazc/blog', target:'_blank' },
+    ],
     locales: {
       '/en/': {
         selectText: 'Languages',
@@ -33,6 +81,7 @@ module.exports = {
         }
       },
       '/': {
+        lastUpdated: '更新时间',
         selectText: '选择语言',
         label: '简体中文',
         editLinkText: '在 GitHub 上编辑此页',
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,1 @@
-[部署](/) <!-- 跳转到根部的 README.md -->
-[article](/article/) <!-- 跳转到 foo 文件夹的 index.html -->
-[foo heading](./#heading) <!-- 跳转到 foo/index.html 的特定标题位置 -->
-[bar - three](../bar/three.md) <!-- 具体文件可以使用 .md 结尾（推荐） -->
-[bar - four](../bar/four.html) <!-- 也可以用 .html -->
-
-::: warning 哈哈
-张思明是sb
-:::
+# 主页
