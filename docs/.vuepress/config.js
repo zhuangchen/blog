69aca8119e48949c7384bf6489e1bfad40dbd2b2
zhuangchen <zhuangchen@zto.cn>
2019-10-23 09:38:58 +0800

    修改部署信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,48 @@
 module.exports = {
-  title: 'vuepress',
-  description: '试一下',
   base: '/',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
   markdown: {
     lineNumbers: true  // 代码行号显示
   },
   locales: {
-   // 键名是该语言所属的子路径
-   // 作为特例，默认语言可以使用 '/' 作为其路径。
-   '/en/': {
-     lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
-     title: 'VuePress',
-     description: 'Vue-powered Static Site Generator'
-   },
-   '/': {
-     lang: 'zh-CN',
-     title: 'VuePress',
-     description: 'Vue 驱动的静态网站生成器'
-   }
- }
+    '/en/': {
+      lang: 'en-US',
+      title: 'en',
+      description: 'Blog Built By Vuepress'
+    },
+    '/': {
+      lang: 'zh-CN',
+      title: '中文',
+      description: 'vuepress搭建的博客'
+    }
+  },
+  themeConfig: {
+    locales: {
+      '/en/': {
+        selectText: 'Languages',
+        label: 'English',
+        ariaLabel: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        }
+      },
+      '/': {
+        selectText: '选择语言',
+        label: '简体中文',
+        editLinkText: '在 GitHub 上编辑此页',
+        serviceWorker: {
+          updatePopup: {
+            message: "发现新内容可用.",
+            buttonText: "刷新"
+          }
+        }
+      }
+    }
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -3,3 +3,7 @@
 [foo heading](./#heading) <!-- 跳转到 foo/index.html 的特定标题位置 -->
 [bar - three](../bar/three.md) <!-- 具体文件可以使用 .md 结尾（推荐） -->
 [bar - four](../bar/four.html) <!-- 也可以用 .html -->
+
+::: warning 哈哈
+张思明是sb
+:::
