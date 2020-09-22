60cb88344f2c0d524509a26e594e3aac1a29ca7b
zhuangchen <zhuangchen@laiyifen.com>
2020-09-22 10:02:03 +0800

    refactor:修改自动发布配置文件

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:babybazc/blog.git master:gh-pages
+git push -f git@github.com:zhuangchen/blog.git master:gh-pages
 
 cd -
